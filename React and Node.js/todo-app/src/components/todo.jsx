44b31b4bc6a139d4a8d95de89090adabf034d2ee
Suraj7149 <santoshsuraj7149@gmail.com>
2025-06-30 23:53:11 +0530

    Update the text input functionality and list to append the item/task to the list

--- React and Node.js/todo-app/src/components/todo.jsx
+++ React and Node.js/todo-app/src/components/todo.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import todo_icon from 'C:/Users/Suraj/Documents/Work/React and Node.js/todo-app/src/assets/todo_icon.png'
+import TodoItems from './TodoItems'
+
+
 const Todo = () => {
+
+const [todolist, setTodoList] = useState([]);
+
+const inputRef = useRef();
+
+const add = () => {
+    // Functionality to add a todo item
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false 
+    }
+
+    setTodoList((prev)=> [...prev, newTodo]);
+    inputRef.current.value= "";
+  } 
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
       
@@ -12,6 +38,24 @@ const Todo = () => {
     </div>
 
 {/* Input Box */}
+
+    <div className='flex item-center my-7 bg-gray-200 rounded-full'>
+      <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task...'/>
+      <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 test-white text-lg font-medium cursor-pointer'>ADD +</button>
+    </div>
+
+{/* ToDo List */}
+
+    <div>
+      {todolist.map((item, index)=> {
+        return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete}/>
+      })}
+
+      {/* <TodoItems text="Learn React"/>
+      <TodoItems text="Learn Node.js"/> */}
+        
+
+    </div>
 
     </div>
   )
