17f0189733e1497f41041baf997aa36acf1ab5b9
Suraj7149 <santoshsuraj7149@gmail.com>
2025-06-30 13:45:21 +0530

    Making a New Repo coz i messed up

---
+++ React and Node.js/todo-app/src/components/todo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import todo_icon from 'C:/Users/Suraj-Home/Documents/Work/React and Node.js/To-Do-App/To-Do-App/src/assets/todo_icon.png'
+const Todo = () => {
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+      
+{/* Title */}
+
+    <div className='flex items-center mt-7 gap-2'>
+        <img className='w-8' src={todo_icon} alt="" />
+        <h1 className='text-3xl font-semibold'>To-Do List</h1>
+    </div>
+
+{/* Input Box */}
+
+    </div>
+  )
+}
+
+export default Todo
