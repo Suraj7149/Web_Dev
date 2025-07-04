b9282b8e9e07fdac167914e208a876da7e6db79b
Suraj7149 <santoshsuraj7149@gmail.com>
2025-07-05 00:32:00 +0530

    Zen mode activated

--- React and Node.js/advanced-todo-app/src/Components/Todo.jsx
+++ React and Node.js/advanced-todo-app/src/Components/Todo.jsx
@@ -3,14 +3,31 @@ import './todo.css'
 
 const Todo = () => {
   return (
-    <div className='todo-app-container'>
-      <input 
-      className='new-task-input' 
-      type="text" 
-      placeholder='Please enter New Task Here...' />
+    <div className='task-container'>
+      <div className='add-new-task'>
+        <input 
+          className='new-task-input' 
+          type="text" 
+          placeholder='Please enter New Task Here...' />
 
-      <button className='add-new-task-button'>+Add</button>
+        <button className='add-new-task-button'>+Add</button>
+      </div>
+      <div className='task-info'>
+        <p className='count'>All Tasks: {21}</p>
+        <p className='count'>Completed Tasks: {10}</p>
+        <p className='count'>Pending Tasks: {11}</p>
+        <button className='Tsk-chn-btn'>{'<'}</button>
+        <p className='count'>{'Home Tasks'}</p>
+        <button className='Tsk-chn-btn'>{'>'}</button>
+      </div>
+      <div className='task-display'>
+        <textarea name="Display All Task Here" id=""></textarea>
+      </div>
+      
+      
     </div>
+
+    
   )
 }
 
