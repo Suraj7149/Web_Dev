6216ac51ffcc16b9b55b755e9591bd99cb14704c
Suraj7149 <santoshsuraj7149@gmail.com>
2025-07-30 05:18:57 +0530

    Hero Section Started

--- React and Node.js/GSAP_Project/src/App.jsx
+++ React and Node.js/GSAP_Project/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger, SplitText);
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
 
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-3xl text-indigo-300'>Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   )
 }
 
---
+++ React and Node.js/GSAP_Project/src/Components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <section id='hero' className='noisy'>
+        <h1 className='title'> 
+            MOJITO
+        </h1>
+
+        <img 
+        src="/images/hero-left-leaf.png" 
+        alt="left-leaf"
+        className="left-leaf"
+        />
+
+         <img 
+        src="/images/hero-right-leaf.png" 
+        alt="right-leaf"
+        className="right-leaf"
+        />
+
+        <div className="body">
+
+        </div>
+
+      </section>
+
+    </>
+  )
+}
+
+export default Hero
