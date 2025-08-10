e832d35539880cb361028d9518e9173aaae7f8f2
Suraj7149 <santoshsuraj7149@gmail.com>
2025-08-11 05:29:42 +0530

    90% there

--- React and Node.js/info_solutions/src/Components/Hero.jsx
+++ React and Node.js/info_solutions/src/Components/Hero.jsx
@@ -8,108 +8,44 @@ const Hero = () => {
   gsap.registerPlugin(MotionPathPlugin);
 
   const [icons, setIcons] = useState([
-    { id: 1, src: "../src/assets/idea.png", alt: "Idea", pos: "0.14" },
-    { id: 2, src: "../src/assets/globe.png", alt: "Globe", pos: "0.27" },
-    { id: 3, src: "../src/assets/nodes.png", alt: "Nodes", pos: "0.40" },
-    { id: 4, src: "../src/assets/s_symbol.png", alt: "S Symbol", pos: "0.53" }, // active one
+    { id: 1, src: "../src/assets/idea.png", alt: "Idea", pos: "0.12" },
+    { id: 2, src: "../src/assets/globe.png", alt: "Globe", pos: "0.255" },
+    { id: 3, src: "../src/assets/nodes.png", alt: "Nodes", pos: "0.39" },
+    { id: 4, src: "../src/assets/s_symbol.png", alt: "S Symbol", pos: "0.51" }, // active one
     { id: 5, src: "../src/assets/service.png", alt: "Service", pos: "0.66" },
-    { id: 6, src: "../src/assets/sharing.png", alt: "Sharing", pos: "0.79" },
-    { id: 7, src: "../src/assets/user.png", alt: "User", pos: "0.92" }
+    { id: 6, src: "../src/assets/sharing.png", alt: "Sharing", pos: "0.795" },
+    { id: 7, src: "../src/assets/user.png", alt: "User", pos: '0.93' }
+
   ]);
 
   const [activeId, setActiveId] = useState(4);
 
-  
-  const icon_sizes = [
-    { height: 50,  width: 50, },
-    { height: 50,  width: 50, },
-    { height: 55,  width: 55, },
-    { height: 70,  width: 70, },
-    { height: 55,  width: 55, },
-    { height: 50,  width: 50, },
-    { height: 50,  width: 50, }
+
+  const step = 0.13; // Each icon's step distance
+  const startOffset = 0.11;
+  const midOffset = 0.51;
+  const endOffset = 0.93;
+
+    const finalPositions = [
+    { top: "150px", left: "-15px" },
+    { top: "58px", left: "45px" },
+    { top: "-13px", left: "135px" },
+    { top: "-40px", left: "250px" },
+    { top: "-13px", right: "135px" },
+    { top: "58px", right: "45px" },
+    { top: "150px", right: "-15px" }
   ];
 
-  // const handleClick = (clickedId) => {
-  // if (clickedId === 4) {
-      
-  //   } else {
-  //     let diff = 0;
+  const snapToFinalPositions = () => {
+    document.querySelectorAll(".icon-holder").forEach((icon, i) => {
+      gsap.set(icon, {
+        clearProps: "transform", // remove translate/scale
+        ...finalPositions[i]
+      });
+    });
+  };
 
-  //     if (clickedId > 4) {
-  //       diff = clickedId - 4;
-  //     } else if (clickedId < 4) {
-  //       diff = 4 - clickedId;
-  //     }
-
-  //     console.log("Diff from 4:", diff); 
-  //   }
-  // };
-
-  
- 
-//   useEffect(() => {
-//   const step = 0.14;     // gap between positions
-//   const offsetBase = 0.05;
-//   const centerPos = 0.47;
-
-//   const icons = document.querySelectorAll(".icon-holder");
-
-//   icons.forEach((icon, clickedIndex) => {
-//     icon.addEventListener("click", () => {
-//       // Find icon currently at the center
-//       const topIcon = Array.from(icons).find(el => el.dataset.pos == centerPos);
-//       const clickedIcon = icon;
-
-//       if (clickedIcon === topIcon) return; // no swap if clicking the top
-
-//       // Calculate clicked icon's current offset
-//       const clickedOffset = (clickedIndex * step) + offsetBase;
-
-//       // Animate top icon from center to clicked offset
-//       gsap.to(topIcon, {
-//         duration: 1.2,
-//         motionPath: {
-//           path: "#motionPath",
-//           align: "#motionPath",
-//           alignOrigin: [0.28, 0.52],
-//           autoRotate: false,
-//           start: centerPos,
-//           end: clickedOffset
-//         },
-//         ease: "power1.inOut",
-//         onComplete: () => {
-//           topIcon.dataset.pos = clickedOffset; // update position
-//         }
-//       });
-
-//       // Animate clicked icon from its current offset to center
-//       gsap.to(clickedIcon, {
-//         duration: 1.2,
-//         motionPath: {
-//           path: "#motionPath",
-//           align: "#motionPath",
-//           alignOrigin: [0.28, 0.52],
-//           autoRotate: false,
-//           start: clickedOffset,
-//           end: centerPos
-//         },
-//         ease: "power1.inOut",
-//         onComplete: () => {
-//           clickedIcon.dataset.pos = centerPos; // update position
-//         }
-//       });
-//     });
-//   });
-
-// }, []);
-     
-const step = 0.13; // Each icon's step distance
-const startOffset = 0.12;
-const midOffset = 0.50;
-const endOffset = 0.92;
-
-const handleIconClick = (clickedIndex) => {
+  const handleIconClick = (clickedIndex) => {
   const clickedId = icons[clickedIndex].id;
   const activeIndex = icons.findIndex(icon => icon.id === activeId);
 
@@ -118,39 +54,51 @@ const handleIconClick = (clickedIndex) => {
   const activeIcon = document.querySelector(".icon-holder.active");
   const clickedIcon = document.querySelectorAll(".icon-holder")[clickedIndex];
 
-  const clickedOffset = startOffset + clickedIndex * step;
-  const activeOffset = startOffset + activeIndex * step;
+  const clickedOffset = icons[clickedIndex].pos;
+  const activeOffset = icons[activeIndex].pos;
 
   const tl = gsap.timeline({
     onComplete: () => {
-      setIcons((prev) => {
-        const updated = [...prev];
-        [updated[clickedIndex], updated[activeIndex]] = [updated[activeIndex], updated[clickedIndex]];
-        return updated;
-      });
-      setActiveId(clickedId); // update active tracking
+      // This is where you replace the state update
+      setIcons(prev =>
+        prev.map((icon, i) =>
+          i === clickedIndex
+            ? { ...icon, pos: prev[activeIndex].pos }
+            : i === activeIndex
+            ? { ...icon, pos: prev[clickedIndex].pos }
+            : icon,
+
+            console.log(
+              "Active Index Pos:", prev[activeIndex].pos,
+              "Clicked Index Pos:", prev[clickedIndex].pos
+            )
+        )
+      );
+      setActiveId(prev => icons[clickedIndex].id);
+      snapToFinalPositions();
     }
-  });
+    });
+
 
   tl.to(clickedIcon, {
-    duration: 1.4,
+    duration: 1.2,
     motionPath: {
       path: "#motionPath",
       align: "#motionPath",
-      alignOrigin: [0.28, 0.52],
+      alignOrigin: [0.58, 0.48],
       start: clickedOffset,
-      end: startOffset + 3 * step // always the middle pos
+      end: midOffset // always the middle pos
     },
     ease: "power1.inOut"
   }, 0);
 
   tl.to(activeIcon, {
-    duration: 1.4,
+    duration: 1.2,
     motionPath: {
       path: "#motionPath",
       align: "#motionPath",
-      alignOrigin: [0.28, 0.52],
-      start: activeOffset,
+      alignOrigin: [0.58, 0.48],
+      start: midOffset,
       end: clickedOffset
     },
     ease: "power1.inOut"
@@ -188,87 +136,22 @@ const handleIconClick = (clickedIndex) => {
         <div className='circular-menu'>
           <div  className='circular-menu-line'>
 
-            {
-            /* <div className='icon-holder'>
-
-            <img 
-              id='1'
-              className={`icon `} 
-                
-              src="../src/assets/idea.png" 
-              alt="" />
-            </div>
-
-            <div className='icon-holder'>
-              <img 
-              id='2'
-              className={`icon `} 
-                
-              src="../src/assets/globe.png" 
-              alt="" />
-            </div>
-            
-            <div className='icon-holder '>
-            <img 
-              id='3'
-              className={`icon`} 
-                
-              src="../src/assets/nodes.png" 
-              alt="" />
-            </div>
-
-            <div className='icon-holder active'>
-            <img 
-              id='4'
-              data-pos="0.47"
-              className={`icon `} 
-              src="../src/assets/s_symbol.png" 
-              alt="" />
-            </div>
-
-            <div className='icon-holder'>
-            <img 
-              id='5'
-              className={`icon`} 
-                
-              src="../src/assets/service.png" 
-              alt="" />
-            </div>
-
-            <div className='icon-holder'>
-            <img 
-              id='6'
-              className={`icon`} 
-               
-              src="../src/assets/sharing.png" 
-              alt="" />
-            </div>
-
-            <div className='icon-holder'>
-            <img 
-              id='7'
-              className={`icon `} 
-                
-              src="../src/assets/user.png" 
-              alt="" />
-            </div> */}
-
             <div className="icon-wrapper">
-  {icons.map((icon) => (
-    <div
-      key={icon.id}
-      className={`icon-holder ${icon.id === activeId ? "active" : ""}`}
-      onClick={() => handleIconClick(icons.findIndex(i => i.id === icon.id))}
-    >
-      <img
-        id={icon.id}
-        className="icon"
-        src={icon.src}
-        alt={icon.alt}
-      />
-    </div>
-  ))}
-</div>
+              {icons.map((icon) => (
+              <div
+                  key={icon.id}
+                  className={`icon-holder ${icon.id === activeId ? "active" : ""}`}
+                  onClick={() => handleIconClick(icons.findIndex(i => i.id === icon.id))}
+              >
+              <img
+                  id={icon.id}
+                  className="icon"
+                  src={icon.src}
+                  alt={icon.alt}
+              />
+              </div>
+            ))}
+          </div>
 
 
           <svg width="590" height="225" viewBox="0 0 590 225" > 
