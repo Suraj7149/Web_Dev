472eb4aa4b91310edab46f8bb311de1adec4fef8
Suraj7149 <santoshsuraj7149@gmail.com>
2025-08-15 07:38:06 +0530

    Knowledge Base for Company - Started

--- React and Node.js/info_solutions/src/Components/Hero.jsx
+++ React and Node.js/info_solutions/src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
     { id: 1, src: "../src/assets/idea.png", alt: "Idea", pos: "0.11" },
     { id: 2, src: "../src/assets/globe.png", alt: "Globe", pos: "0.255" },
     { id: 3, src: "../src/assets/nodes.png", alt: "Nodes", pos: "0.39" },
-    { id: 4, src: "../src/assets/s_symbol.png", alt: "S Symbol", pos: "0.52 " }, // active one
+    { id: 4, src: "../src/assets/s_symbol.png", alt: "S Symbol", pos: "0.52" }, // active one
     { id: 5, src: "../src/assets/service.png", alt: "Service", pos: "0.66" },
     { id: 6, src: "../src/assets/sharing.png", alt: "Sharing", pos: "0.795" },
     { id: 7, src: "../src/assets/user.png", alt: "User", pos: '0.93' }
@@ -85,7 +85,7 @@ const Hero = () => {
     motionPath: {
       path: "#motionPath",
       align: "#motionPath",
-      alignOrigin: [0.58, 0.48],
+      alignOrigin: [clickedIndex < 4 ? 0.65 : 0.5,0.7],
       start: clickedOffset,
       end: midOffset // always the middle pos
     },
@@ -125,12 +125,12 @@ const Hero = () => {
   return (
     <div className="hero-container">
         <div className="heading">
-            <h3><span>DIGITAL</span> SOLUTIONS THAT <br />SCALE</h3>
-            <h1>
+            <h1><span>DIGITAL</span> SOLUTIONS THAT <br />SCALE</h1>
+            <h3>
                 Empowering utilities, municipalities, and enterprises to modernize,
                 <br />integrate, and grow through agile, people-focused digital
                 <br />transformation.
-            </h1>
+            </h3>
         </div>
         
         <div className='circular-menu'>
@@ -163,10 +163,63 @@ const Hero = () => {
            />
           </svg>
 
+          <div className='description-heading'>
+            <div className='idea-png-desc desc_hd active'>
+              <h1><span className='headingspan'>Solutions</span><br />ARCHITECTURE</h1>
+              <h3>Design and deliver technological solutions to address specific business  
+                <br />challenges, ensuring they align with the organizations overall IT strategy.</h3>
+              <div className='cards active'>
+                <div className='card card1 active'>
+                  <img src="../src/assets/s_symbol.png" alt="" />
+                  <label htmlFor="">What we offer</label>
+                  <ul>
+                    <li><span className='card_span'>Strategic Alignment: </span>Solutions</li>
+                    <li><span className='card_span'>Integrated Approach: </span>Seamlessly </li>
+                    <li><span className='card_span'>Scalability at Core: </span>Built</li>
+                  </ul>
+                </div>
+                
+              </div>
+            </div>
+
+            <div className='globe-png-desc'>
+              <h1><span>Growth</span><br />READY</h1>
+              <h3>Prepare for scale from day one.</h3>
+            </div>
+
+            <div className='nodes-png-desc'>
+              <h1><span>Digital</span><br />Self-Service</h1>
+              <h3>Delivering unified, personalized digital experiences across web and mobile platforms.</h3>
+            </div>
+
+            <div className='s_symbol-png-desc'>
+              <h1>Integration</h1>
+              <h3>Streamline operations and scale with seamless system integration and enterprise <br />connectivity.</h3>
+            </div>
+
+            <div className='serivce-png-desc'>
+              <h1><span>Maintenance</span><br />Support</h1>
+              <h3>Dedicated maintenance support designed to your evolving business needs.</h3>
+            </div>
+
+            <div className='sharing-png-desc'>
+              <h1><span>DevOps</span><br />Consulting</h1>
+              <h3>Strategic DevOps and managed services to ensure speed, stability, and security across 
+                <br />all development stages</h3>
+            </div>
+
+            <div className='user-png-desc'>
+              <h1><span>specialized</span><br />Talent Acquisition</h1>
+              <h3>Delivering vetted, domain-aligned talent that fits your culture and meets technical 
+                <br />needs</h3>
+            </div>
+
+            
+            
+
           </div>
 
-
-          
+          </div>        
 
 
           
